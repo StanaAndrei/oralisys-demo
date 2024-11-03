0b02a6b19daa9ad7b3bb4ce740cf9d31fdd6281d
StanaAndrei <stadey33@gmail.com>
2024-11-03 11:51:03 +0200

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import QuestionList from './components/QuestionList.jsx';
 import PersonalInfoForm from './components/PersonalInfoForm.jsx';
-import {useState} from 'react';
-import {questionsData} from '../datastore/questions-pacient-state.js';
+import { useState } from 'react';
+import { questionsData } from '../datastore/questions-pacient-state.js';
+import Logo from './assets/logo.png'
 
 function App() {
-    const [personalInfo, setPersonalInfo] = useState(null);
+  const [personalInfo, setPersonalInfo] = useState(null);
 
-    const handleFormFinalSubmit = (questionListAnswersForm) => {
-        console.log({
-            personalInfo,
-            questionListAnswers: questionListAnswersForm
-        });
-    };
+  const handleFormFinalSubmit = (questionListAnswersForm) => {
+    console.log({
+      personalInfo,
+      questionListAnswers: questionListAnswersForm
+    });
+  };
 
-    return (
-        <div className="max-w-lg mx-auto p-4">
-            {
-                personalInfo ?
-                    <QuestionList questionsData={questionsData} handleFormFinalSubmit={handleFormFinalSubmit} />:
-                    <PersonalInfoForm handleOnPersonalInfoFormSubmited={setPersonalInfo}/>
-            }
-        </div>
-    );
+  return (
+    <>
+     <img src={Logo} style={{}} />
+      <div className="max-w-lg mx-auto p-4">
+        {
+          personalInfo ?
+            <QuestionList questionsData={questionsData} handleFormFinalSubmit={handleFormFinalSubmit} /> :
+            <PersonalInfoForm handleOnPersonalInfoFormSubmited={setPersonalInfo} />
+        }
+      </div>
+    </>
+  );
 }
 
 export default App;
