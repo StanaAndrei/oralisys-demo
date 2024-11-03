8aa65d8be5feb6994f348dea81332f71efdbb59a
vrotariu1 <vladrotariu24@gmail.com>
2024-11-03 15:32:28 +0200

    FEAT: Add loading states

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import PersonalInfoForm from './components/PersonalInfoForm.jsx';
 import { useState } from 'react';
 import { questionsData } from '../datastore/questions-pacient-state.js';
 import Logo from './assets/logo-blue.png';
+import SuccessPage from './components/SuccessPage.jsx';
+import Spinner from './components/Spinner.jsx';
 
 function App() {
     const [personalInfo, setPersonalInfo] = useState(null);
+    const [hasFormBeenSubmited, setHasFormBeenSubmited] = useState(false);
+    const [isSpinnerLoading, setIsSpinnerLoading] = useState(false);
 
     const handleFormFinalSubmit = (questionListAnswersForm) => {
         console.log({
             personalInfo,
             questionListAnswers: questionListAnswersForm
         });
+        setHasFormBeenSubmited(true);
+        setIsSpinnerLoading(true);
+        setTimeout(() => setIsSpinnerLoading(false), 3000);
     };
 
     return (
@@ -22,27 +29,35 @@ function App() {
                         <img className="w-16 md:w-24 lg:w-32 xl:w-full mb-8"
                             src="https://landingfoliocom.imgix.net/store/collection/saasui/images/newsletter/3/ring-pattern.svg"
                             alt=""/>
+                        <img src={Logo} alt="Oralisys Logo" className="mx-auto mb-16" style={{width: '70%'}}/>
 
-                        <div className="relative px-8 py-12 md:p-16 xl:p-24">
-                            <img src={Logo} alt="Oralisys Logo" className="mx-auto mb-32" style={{width: '70%'}}/>
+                        {
+                            hasFormBeenSubmited ?
+                                ( isSpinnerLoading? <Spinner /> : <SuccessPage /> ) :
+                                <>
+                                    <div className="relative px-8 py-12 md:p-16 xl:p-24">
+                                        <div className="max-w-2xl mx-auto text-center">
+                                            <h2 className="text-3l font-semibold tracking-tight text-black sm:text-4xl lg:text-5xl">Chestionar
+                                            pentru starea de sanatate</h2>
+                                            <p className="mt-4 text-base font-normal leading-7 text-gray-400 lg:text-lg lg:mt-6 lg:leading-8">
+                                            Revoluționează experiența pacienților tăi! Completează documentele
+                                            stomatologice
+                                            instant,
+                                            prin scanarea unui singur cod QR.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="max-w-lg mx-auto p-4 bg-white rounded-3xl">
+                                        {
+                                            personalInfo ?
+                                                <QuestionList questionsData={questionsData}
+                                                    handleFormFinalSubmit={handleFormFinalSubmit}/> :
+                                                <PersonalInfoForm handleOnPersonalInfoFormSubmited={setPersonalInfo}/>
+                                        }
+                                    </div>
+                                </>
+                        }
 
-                            <div className="max-w-2xl mx-auto text-center">
-                                <h2 className="text-3l font-semibold tracking-tight text-black sm:text-4xl lg:text-5xl">Chestionar
-                                    pentru starea de sanatate</h2>
-                                <p className="mt-4 text-base font-normal leading-7 text-gray-400 lg:text-lg lg:mt-6 lg:leading-8">
-                                    Revoluționează experiența pacienților tăi! Completează documentele stomatologice instant,
-                                    prin scanarea unui singur cod QR.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="max-w-lg mx-auto p-4 bg-white rounded-3xl">
-                            {
-                                personalInfo ?
-                                    <QuestionList questionsData={questionsData}
-                                        handleFormFinalSubmit={handleFormFinalSubmit}/> :
-                                    <PersonalInfoForm handleOnPersonalInfoFormSubmited={setPersonalInfo}/>
-                            }
-                        </div>
                     </div>
                 </div>
             </section>
---
+++ src/components/SuccessPage.jsx
@@ -0,0 +1,16 @@
+function SuccessPage() {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-6xl font-extrabold text-blue-600 mb-6">
+                    Multumim!
+                </h1>
+                <p className="text-xl text-gray-500">
+                    Operatiunea a fost inregistrata cu succes!
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default SuccessPage;
